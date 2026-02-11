af378ad990877fece82135c95b16cd5f5e12154a
Shannon Morrissey <smorrissey3@gmail.com>
2026-02-11 00:05:46 -0600

    a haon

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
+//20260211T060343Z
+
 function setup() {
     createCanvas(800, 600);
 }
 
 function draw() {
     background(220);
+    textAlign(CENTER, CENTER);
+    text("deasghnath", width / 2, height / 2);
 }
